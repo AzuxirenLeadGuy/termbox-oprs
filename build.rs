03d982f70247404d32c0e92b733163b1b21757b1
AzuxirenLeadGuy <sam4lordjesus@outlook.com>
2026-08-26 15:27:43 +0100

    adding saner link strategy

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
-fn main(){
-    println!("cargo::rustc-link-search=native=./build/lib")
+fn main() {
+    let env_var = "LINK_TERMBOX_OP";
+    let link_path = std::env::var(env_var);
+    if let Ok(value) = link_path {
+        println!("cargo::rustc-link-search=native={}", value);
+    }
 }
