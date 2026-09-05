e8b3b4f4d4611a43d9d0eb1320e3820d90cf39ba
AzuxirenLeadGuy <sam4lordjesus@outlook.com>
2026-09-05 21:52:39 +0100

    removed redundancies

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let env_var = "LINK_TERMBOX_OP";
     let shared_link_type = std::env::var("CARGO_FEATURE_TERMBOX_SHARED_LINK").is_ok();
-    let link_path = std::env::var(env_var);
-    if let Ok(value) = link_path {
+    if let Ok(value) = std::env::var("LINK_TERMBOX_OP") {
         println!("cargo::rustc-link-search=native={}", value);
     }
     let libname = "termboxop";
